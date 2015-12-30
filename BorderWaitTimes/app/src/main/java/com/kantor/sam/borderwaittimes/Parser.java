284e23e9f249639e41bf2eefc599ce52b5639fee
Sam Kantor <sam.kantor@gmail.com>
2015-12-30 15:30:03 -0500

    Cleaned up and removed unnecessary lines

--- BorderWaitTimes/app/src/main/java/com/kantor/sam/borderwaittimes/Parser.java
+++ BorderWaitTimes/app/src/main/java/com/kantor/sam/borderwaittimes/Parser.java
@@ -3,15 +3,9 @@ package com.kantor.sam.borderwaittimes;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
 import java.net.URL;
 
 import org.jsoup.Jsoup;
-
 
 /**
  * Created by sam on 12/30/2015.
@@ -30,29 +24,13 @@ public class Parser extends AsyncTask <Void, Void, Void>
     }
 
     @Override
-    protected Void doInBackground(Void... params)
+    protected Void doInBackground(Void ... params)
     {
         try
         {
-            URL url = new URL("https://apps.cbp.gov/bwt/mobile.asp?action=n&pn=0901");
-            Document doc = (Document) Jsoup.connect("https://apps.cbp.gov/bwt/mobile.asp?action=n&pn=0901").get();
-
-            Element container = doc.getElementById("container");
-            Element pass_details = doc.getElementById("pass_details");
-            NodeList nodeList = doc.getElementsByTagNameNS("pass_details", "b");
-
-
-            Node el1 = nodeList.item(1);
-            Bridge1_time = el1.getTextContent();
-        /*
-        Element el1 = (Element)nodeList.item(4);
-        Element el2 = (Element)nodeList.item(5);
-        Element el3 = (Element)nodeList.item(6);
-
-        Bridge1_time = el1.getTextContent();
-        Bridge2_time = el2.getTextContent();
-        Bridge3_time = el3.getTextContent();
-        */
+            org.jsoup.nodes.Document doc = Jsoup.connect("https://apps.cbp.gov/bwt/mobile.asp?action=n&pn=0901").get();
+            org.jsoup.nodes.Element pass_details = doc.select("div.pass_details").select("b").get(1);
+            Bridge1_time = pass_details.text();
         }
         catch (Exception e)
         {
@@ -60,9 +38,11 @@ public class Parser extends AsyncTask <Void, Void, Void>
         }
         return null;
     }
-    @Override
-    protected void onPostExecute(Void result) {
+
+    protected void onPostExecute(Void result)
+    {
         super.onPostExecute(result);
+        Bridge1_time = "Delay is: " + Bridge1_time;
     }
 
 }
