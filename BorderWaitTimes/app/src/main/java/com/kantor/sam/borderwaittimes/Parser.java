daadf3c3d5b2a68c32ad90d87d766cf4818b5865
Sam Kantor <sam.kantor@gmail.com>
2015-12-30 13:33:28 -0500

    Removed DB Factory and implemented JSOUP Implemented AsyncTask Methods to allow UI thread to run parallel to network threads

--- BorderWaitTimes/app/src/main/java/com/kantor/sam/borderwaittimes/Parser.java
+++ BorderWaitTimes/app/src/main/java/com/kantor/sam/borderwaittimes/Parser.java
@@ -1,34 +1,50 @@
 package com.kantor.sam.borderwaittimes;
 
+import android.os.AsyncTask;
+import android.widget.Toast;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 
 import java.net.URL;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import org.jsoup.Jsoup;
+
 
 /**
  * Created by sam on 12/30/2015.
  */
 
-public class Parser{
+public class Parser extends AsyncTask <Void, Void, Void>
+{
     public String Bridge1_time = "no delay";
     public String Bridge2_time = "no delay";
     public String Bridge3_time = "no delay";
 
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Bridge1_time = "Updating";
+    }
 
-    public void getXMLelementsfromURL() throws Exception
+    @Override
+    protected Void doInBackground(Void... params)
     {
-        URL url = new URL ("https://apps.cbp.gov/bwt/mobile.asp?action=n&pn=0901");
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        Document doc = db.parse(new InputSource(url.openStream()));
-        doc.getDocumentElement().normalize();
-        NodeList nodeList = doc.getElementsByTagName("basic_text text_margin");
+        try
+        {
+            URL url = new URL("https://apps.cbp.gov/bwt/mobile.asp?action=n&pn=0901");
+            Document doc = (Document) Jsoup.connect("https://apps.cbp.gov/bwt/mobile.asp?action=n&pn=0901").get();
 
+            Element container = doc.getElementById("container");
+            Element pass_details = doc.getElementById("pass_details");
+            NodeList nodeList = doc.getElementsByTagNameNS("pass_details", "b");
+
+
+            Node el1 = nodeList.item(1);
+            Bridge1_time = el1.getTextContent();
+        /*
         Element el1 = (Element)nodeList.item(4);
         Element el2 = (Element)nodeList.item(5);
         Element el3 = (Element)nodeList.item(6);
@@ -36,6 +52,17 @@ public class Parser{
         Bridge1_time = el1.getTextContent();
         Bridge2_time = el2.getTextContent();
         Bridge3_time = el3.getTextContent();
+        */
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
     }
 
 }
